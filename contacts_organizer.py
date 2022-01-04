e64a9334a5a901b78e9bf0f5f21f93e8ce5d60ec
Richard Lu <richardlu0928@gmail.com>
2022-01-03 19:37:55 -0500

    Optimized overall performance, added base64 encoding.

--- contacts_organizer.py
+++ contacts_organizer.py
@@ -2,72 +2,95 @@
 # Author: Richard Lu
 # Project name: Contacts Organizer
 # Date Started: October 2020
-# Contact: richardlu0928@gmail.com
-# Total Time used: 5 hours
-# Last Updated: 2020/12/29
-# Version: 1.0
+# Contact: admin@richardlu.ca
+# Repo: https://github.com/Sprit-Fun/Contacts_Organizer
+# Total Time used: 6 hours
+# Last Updated: 2022/01/03
+# Version: 1.01
 """
 
 # This program can add contacts, delete contacts, search contacts and list
-# out all contacts stored locally.
-# Run the program with IDLE to edit contacts locally.
+# out all contacts stored locally, encrypted with base64 for extra security.
+# Run the program with any Python interpreter to edit contacts locally.
 
+import base64
 import os
-from typing import List, Tuple, Dict, TextIO
 from time import sleep
+from typing import Dict, List, TextIO, Tuple
+
+
+def encoding(info: str) -> str:
+    info_bytes = info.encode("ascii")
+    base64_bytes = base64.b64encode(info_bytes)
+    encrypted = base64_bytes.decode("ascii")
+    return encrypted
+
+
+def decoding(info: str) -> str:
+    base64_bytes = info.encode("ascii")
+    info_bytes = base64.b64decode(base64_bytes)
+    decrypted = info_bytes.decode("ascii")
+    return decrypted
+
+
+def print_i(chunk: str) -> None:
+    dataed = chunk.split("\n")
+    for i in dataed:
+        print(decoding(i))
+    print()
 
 
 def add() -> None:
     name = input("Please input new contact's name: ")
     number = input("Please input new contact's number: ")
-    address = input("Please input new contact's address. Simply press Enter "
+    address = input("Please input new contact's address. Simply hit Enter "
                     "if not applicable: ")
-    birth_date = input("Please input new contact's birth date. Simply press "
+    birth_date = input("Please input new contact's birth date. Simply hit "
                        "Enter if not applicable: ")
     if address == '':
         address = '@!'
     if birth_date == '':
-        birth_date = 'Birthday Unknown'
-    concatenated = name + '\n' + number + '\n' + address + '\n' + birth_date
-    file_contacts = 'contacts.txt'
-    openfile = open(file_contacts, 'a')
+        birth_date = 'No birthday added'
+    concatenated = encoding(name) + '\n' + encoding(number) + '\n' + \
+                   encoding(address) + '\n' + encoding(birth_date)
+    openfile = open('contacts', 'a')
     openfile.write(concatenated + '\n\n')
     openfile.close()
-    print("New contact successfully added.")
+    print("New contact successfully added. \n")
     run()
 
 
 def search() -> None:
+    data = open('contacts', 'r').read()
+    data_array = data.split("\n\n")
+    if data_array == ['']:
+        print('File is empty with no contacts, please add one first. \n')
+        run()
+    del data_array[-1]
     mode_1 = input("Type 1 to search by name, type 2 to search by phone "
                    "number, type 3 to search by address, type anything "
                    "else to exit the program: ")
-    data = open('contacts.txt', 'r').read()
-    data_array = data.split("\n\n")
-    if data_array == ['']:
-        print('File is empty with no contacts, please add one first.')
-        run()
-    del data_array[-1]
     if mode_1 == "1":
         name = []
         for i in data_array:
             listed = i.split('\n')
-            name.append(listed[0])
+            name.append(decoding(listed[0]))
         _search(name, data_array, "name")
     elif mode_1 == "2":
         number = []
         for i in data_array:
             listed = i.split('\n')
-            number.append(listed[1])
+            number.append(decoding(listed[1]))
         _search(number, data_array, "number")
     elif mode_1 == "3":
         print("Input @! to search all contacts without address inputted.")
         address = []
         for i in data_array:
             listed = i.split('\n')
-            address.append(listed[2])
+            address.append(decoding(listed[2]))
         _search(address, data_array, "address")
     else:
-        print("Program exited. Thanks for using.")
+        print("Program exited. Thanks for using. \n")
 
 
 def _search(search_spec: list, main_list: list, which: str) -> None:
@@ -80,18 +103,18 @@ def _search(search_spec: list, main_list: list, which: str) -> None:
         for j in search_spec:
             if msg.lower().replace(" ", "") in j.lower().replace(" ", ""):
                 print("Contact Number " + str(counter) + ": ")
-                print(main_list[counted])
-                print()
+                print_i(main_list[counted])
                 counter += 1
             counted += 1
-        print("All data has been printed out.")
+        print("All contacts have been printed out. \n")
         run()
     else:
-        print("Sorry, can't find this contact. Please restart the program.")
+        print("Sorry, can't find this contact. \n")
+        run()
 
 
 def list() -> None:
-    data = open('contacts.txt', 'r').read()
+    data = open('contacts', 'r').read()
     data_array = data.split("\n\n")
     _list(data_array)
     run()
@@ -99,19 +122,19 @@ def list() -> None:
 
 def _list(data_array) -> None:
     if data_array == ['']:
-        print('File is empty with no contacts, please add a contact first.')
+        print('File is empty with no contacts, please add a contact first. \n')
         run()
     else:
         counter = 1
         for i in data_array[:-1]:
             print("Contact number " + str(counter) + ":")
-            print(i + '\n')
+            print_i(i)
             counter += 1
-        print("All data has been printed out.")
+        print("All data has been printed out. \n")
 
 
 def delete() -> None:
-    data = open('contacts.txt', 'r').read()
+    data = open('contacts', 'r').read()
     data_array = data.split("\n\n")
     _list(data_array)
     numb = input("Which contact number do you wish to delete: ")
@@ -121,21 +144,22 @@ def delete() -> None:
         numb = int(numb) - 1
         del data_array[numb]
         del data_array[-1]
-        with open("contacts.txt", "w") as f:
+        with open("contacts", "w") as f:
             for i in data_array:
                 f.write(i + '\n\n')
         print("Contact number " + str(numb + 1) + " has been deleted "
-                                                  "successfully.")
+                                                  "successfully. \n")
         run()
     else:
+        print()
         run()
 
 
 def run() -> None:
-    mode = input("Type 1 to add a contact, type 2 search a contact, type 3 to "
-                 "list all contacts stored, type 4 to delete a contact, type "
+    mode = input("Input 1 to add a contact, 2 search a contact, 3 to "
+                 "list all contacts stored, 4 to delete a contact, "
                  "anything else to exit the program. "
-                 "Press Enter after inputted: ")
+                 "Press Enter after input: ")
     if mode == '1':
         print("Received. You want to add a contact. \n")
         add()
@@ -149,14 +173,13 @@ def run() -> None:
         print("Received. You want to delete a stored contact. \n")
         delete()
     else:
-        print("Program exited. Thanks for using.")
+        print("Exiting now. Thank you for using!")
         exit()
 
 
 if __name__ == '__main__':
-    print("Welcome to Contacts Organizer, a program made by Richard Lu.")
-    if not os.path.exists('contacts.txt'):
-        with open('contacts.txt', 'w'):  # Create a new local file if not exist
+    print("Welcome to Contacts Organizer V1.01 written by Richard Lu.")
+    if not os.path.exists('contacts'):
+        with open('contacts', 'w'):  # Create a new local file if not exist
             pass
-    sleep(0.5)
-    run()
+    run()  # Start running
